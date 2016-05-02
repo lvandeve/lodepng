cd2d332c91336bbaa1ef7f33babb93b865e86bfe
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2016-05-02 19:49:02 +0200

    disable warning for vs2015

--- lodepng.cpp
+++ lodepng.cpp
@@ -37,6 +37,7 @@ Rename this file to lodepng.cpp to use it for C++, or to lodepng.c to use it for
 #if defined(_MSC_VER) && (_MSC_VER >= 1310) /*Visual Studio: A few warning types are not desired here.*/
 #pragma warning( disable : 4244 ) /*implicit conversions: not warned by gcc -Wall -Wextra and requires too much casts*/
 #pragma warning( disable : 4996 ) /*VS does not like fopen, but fopen_s is not standard C so unusable here*/
+#pragma warning( disable : 4334 ) /*VS does not like fopen, but fopen_s is not standard C so unusable here*/
 #endif /*_MSC_VER */
 
 const char* LODEPNG_VERSION_STRING = "20160418";
